f7880d6dd35efa5c2fbb5a5b578ffa2ac7f770a8
gabang2 <offbeat1020@naver.com>
2022-05-13 10:56:07 +0900

    url

---
+++ accountapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+app_name = "accounts"
+
+urlpatterns = [
+    path('hello/', TemplateView.as_view(template_name='accountapp/hello.html'), name="hello")
+]

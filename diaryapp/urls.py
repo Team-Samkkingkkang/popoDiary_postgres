3ee445d1161a80487f30a05ac3a1d25f9699bf18
gabang2 <offbeat1020@naver.com>
2022-05-14 16:03:03 +0900

    04:02 commit

--- diaryapp/urls.py
+++ diaryapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from diaryapp import views
+
 app_name = "diarys"
 
 urlpatterns = [
-    path('diary/', TemplateView.as_view(template_name='diaryapp/diary.html'), name="diary")
+    path('', TemplateView.as_view(template_name='diaryapp/diary.html'), name="diary"),
+    path('create/', views.create_diary, name="create_diary")
 ]
